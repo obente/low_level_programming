eaff8ba6edeaa7e86da83253a31dd712a5e5b991
obinna ekenonu <obinnaekenonu@gmail.com>
2022-10-13 14:21:43 -0700

    fix to align with betty style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,8 @@
 /**
  *main - print the size of various types
  *
- *Description: prints the size of various types on the computer it is compiled and run on
+ *Description: prints the size of various types on the computer 
+ * it is compiled and run on
  *Return: Always 0 (Success)
  */
 int main(void)
@@ -10,7 +11,8 @@ int main(void)
 	printf("Size of a char: %d byte(s)\n", (int)sizeof(char));
 	printf("Size of an int: %d byte(s)\n", (int)sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", (int)sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n", (int)sizeof(long long int));
+	printf("Size of a long long int: %d byte(s)\n", 
+			(int)sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", (int)sizeof(float));
 	return (0);
 }
