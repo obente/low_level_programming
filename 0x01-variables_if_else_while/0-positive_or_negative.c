d6f2b0a076633434f2190ee7b0bace5838bbcb4e
obinna ekenonu <obinnaekenonu@gmail.com>
2022-10-14 07:43:18 -0700

    fixed betty formatting error

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
  * main - assign a random number to the variable n
  *
  * Description: assign a random number to the variable n
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -15,10 +15,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%d is positive\n",n);
+		printf("%d is positive\n", n);
 	else if (n < 0)
-		printf("%d is negative\n",n);
+		printf("%d is negative\n", n);
 	else
-		printf("%d is zero\n",n);
+		printf("%d is zero\n", n);
 	return (0);
 }
