c2aca6ae30b59cc11720dedc14c37c9b54a3a03d
obinna ekenonu <obinnaekenonu@gmail.com>
2022-10-18 14:40:51 -0700

    4. is_alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if char is uppercase & lowercase
+ *
+ * @c : int type
+ * Return: Always 0 (Success)
+ */
+
+int _isalpha(int c)
+{
+	return (c >= 'A' && c <= 'z');
+}
