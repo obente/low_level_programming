3d1e8e13b3339ddd239397fda734071bc66ed0b5
obinna ekenonu <obinnaekenonu@gmail.com>
2022-10-18 14:27:35 -0700

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - checks if char is lowercase
+ *
+ * @c : int type
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c)
+	return (c >= 'a' && c <= 'z');
+
