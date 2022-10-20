aae242c8856af807da54bb267d3e50fe0046e410
obinna ekenonu <obinnaekenonu@gmail.com>
2022-10-20 04:54:49 -0700

    1. Are you a digit?

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isdigit - checks for digits
+ *
+ * @c: takes an integer parameter
+ *
+ * Return: 1 if digit, 0 if not
+ */
+
+int _isdigit(int c)
+{
+	return (isdigit(c));
+}
