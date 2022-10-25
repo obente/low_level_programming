963282cdf41bc860581902406223272a762cda1b
obinna ekenonu <obinnaekenonu@gmail.com>
2022-10-24 21:46:12 -0700

    4. reverse a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * print_rev - print string
+ * @str: string to be reversed
+ * Return: void
+ */
+
+void print_rev(char *str)
+{
+	puts(strrev(str));
+}
