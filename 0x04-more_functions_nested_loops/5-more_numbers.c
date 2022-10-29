401885f242b821b328474616707df0e5ce86812a
obinna ekenonu <obinnaekenonu@gmail.com>
2022-10-29 13:38:44 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,15 +1,26 @@
 #include "main.h"
 
+/**
+ * more_numbers - print nums 0 - 14
+ * Return: nothing
+ */
+
 void more_numbers(void)
 {
-	int i, j;
+	char z;
+	int i,j;
 
-	for (i = 0; i <= 14; i++)
+	z = 48;
+	for (j = 0; j < 10; j++)
 	{
-		_putchar(i);
-		if (i > 9)
+		for (i = 0; i < 15; i++)
 		{
-			
+			if (i > 9)
+				_putchar('1');
+			_putchar(z++);
+			if (z == 58)
+				z = 48;
 		}
+		_putchar('\n');
 	}
 }
