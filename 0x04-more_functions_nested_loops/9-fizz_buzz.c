dbc1ab222013774e648266e372afaff018af9d27
obinna ekenonu <obinnaekenonu@gmail.com>
2022-10-30 03:58:51 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - fizz buzz
+ * Return: Always (0) Success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+		{
+			printf("Fizz Buzz ");
+			continue;
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+			continue;
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+			continue;
+		}
+		printf("%d ", i);
+	}
+	putchar('\n');
+	return (0);
+}
