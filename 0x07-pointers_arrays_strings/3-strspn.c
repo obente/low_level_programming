85eacf6f9aa0ce1cccb3c07903e7b0bf3eb15828
obinna ekenonu <obinnaekenonu@gmail.com>
2022-10-31 21:44:20 -0700

    implementation change

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,22 +1,29 @@
 #include "main.h"
 
+/**
+ * _strspn - get length of prefix substring
+ * @s: string passed
+ * @accept: accepted bytes
+ * Return: number of accepted bytes
+ */
+
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int c;
-	char *x = accept;
+	unsigned int i, j, c;
 
-	while (*s++)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		while (*accept++)
+		c = 1;
+		for (j = 0; *(accept + j) != '\0'; j++)
 		{
 			if (*(s - 1) == *(accept - 1))
 			{
-				c++;
+				c = 0;
 				break;
 			}
-			if (!(*--accept))
-				break;
-			accept = x;
 		}
-		return (c);
+		if (c == 1)
+			break;
 	}
+	return (c);
+}
