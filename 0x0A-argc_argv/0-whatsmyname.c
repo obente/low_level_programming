9b49136ce2cd5e076a57d138524c2211721c625d
obinna ekenonu <obinnaekenonu@gmail.com>
2022-11-06 03:09:07 -0800

    betty fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - print name of program
+ * @argc: number of command line args
+ * @argv: argument passed
+ * Return: Always(0) Success
+ */
+
 int main(int argc, char *argv[])
 {
 	if (argc == 1)
