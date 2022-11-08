427e75b833889613f57ba77e0bbd08137f0437d1
obinna ekenonu <obinnaekenonu@gmail.com>
2022-11-08 14:04:09 -0800

    create_array Nulls

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,12 +14,12 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-		return ('\0');
+		return (NULL);
 
 	p = malloc(sizeof(c) * size);
 
-	if (*p == '\0')
-		return ('\0');
+	if (p == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		*(p + i) = c;
