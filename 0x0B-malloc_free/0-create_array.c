e8238d0acf26cbc2a579d30f7e4b4667981af9b4
obinna ekenonu <obinnaekenonu@gmail.com>
2022-11-08 08:59:04 -0800

    betty fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,18 +10,18 @@
 
 char *create_array(unsigned int size, char c)
 {
+	char *p;
 	unsigned int i;
-	
+
 	if (size == 0)
 		return ('\0');
-	
-	char *p; 
-       	p = malloc(sizeof(c) * size);
+
+	p = malloc(sizeof(c) * size);
 
 	if (*p == '\0')
 		return ('\0');
 
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		*(p + i) = c;
 
 	return (p);
