50ac24a29697f1a606546c7a4e2860efd8d620d8
obinna ekenonu <obinnaekenonu@gmail.com>
2022-11-09 05:17:44 -0800

    4 add positive int

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+/**
+ * main - a program that adds postive intigers
+ * @argv: args passed
+ * @argc: no of args
+ * Return: Always (0)
+ */
+
+int main(int argc, int **argv)
+{
+	int sum, i, adder;
+	char *n;
+
+	sum = 0;
+	adder = 0;
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		adder = strtol(argv[i], &n, 10);
+		if (*n == '\0')
+			sum = sum + adder;
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	print("%d\n", sum);
+	return (0);
+
+}
