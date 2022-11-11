f1d0b318946f200a727dfe632a00e12370f40977
obinna E <obinnaekenonu@gmail.com>
2022-11-11 01:07:36 +0000

    malloc in action

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * malloc_checked - creates memory using malloc
+ * @b: memory size
+ * Return: pointer to allocated mem.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *m = malloc(sizeof(int) * b);
+	if (m == NULL)
+		return (98);
+	return (m);
+}
