163d9bc7d2911d8b3034686a9a94c1004dec4144
obinna E <obinnaekenonu@gmail.com>
2022-11-14 22:28:54 +0000

    0. poppy with betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - dog details
+ * @name: Name of dog
+ * @age: Dog age
+ * @owner: owner name
+ *
+ * Description: Keep details of dog
+ */
+
 struct dog
 {
 	char *name;
