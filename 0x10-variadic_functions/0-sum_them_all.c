39e63ac2fa0457eb79c7c22839a497a50d1fcd42
obinna E <obinnaekenonu@gmail.com>
2022-11-17 14:55:13 +0000

    sum numbers

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums numbers given
+ * @n: int var
+ * @Return: (0)
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	if (n == 0)
+		return (0);
+
+	else
+	{
+		int i, sum;
+		va_list num;
+
+		va_start(num, n);
+		sum = 0;
+		for (i = 0; i < n; i++)
+		{
+			int a = va_arg(num, int);
+			sum = sum + a;
+		}
+		va_end(num);
+		return sum;
+	}
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,3 @@
+## Welcome to ALX low level programming with C language.
+
+# Variadic functions
