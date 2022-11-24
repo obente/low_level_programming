7378900dcf96fb259f96fbe8cf4858987aa625ee
obeki <obinnaekenonu@gmail.com>
2022-11-24 20:13:47 +0100

    var change to nelem

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,35 +1,19 @@
 #include "lists.h"
-
 /**
- * print_list - prints all the elements of a list_t list.
+ * list_len - returns then number of elements in a list.
  * @h: singly linked list.
  * Return: number of elements in the list.
  */
 
-
-
-size_t print_list(const list_t *h)
-
+size_t list_len(const list_t *h)
 {
+	size_t nelem;
 
-	size_t s;
-
-	s = 0;
-
+	nelem = 0;
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-
-		else
-			printf("[%d] %s\n", h->len, h->str);
-
 		h = h->next;
-
-		s++;
-
+		nelem++;
 	}
-
-	return (s);
-
+	return (nelem);
 }
