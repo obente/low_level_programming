8c61c13551c58099a1ac3d5e18488dd2b25138d1
obinna ekenonu <obinnaekenonu@gmail.com>
2022-11-29 07:40:13 -0800

    corrected

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,7 +7,7 @@
  * Return: nth node
  */
 
-listint_t *get_nodeint_at_index(list_t *head, unsigned int index)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
