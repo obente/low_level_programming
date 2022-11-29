72075fab7ba9a590641129a2c0222339924450b5
obinna ekenonu <obinnaekenonu@gmail.com>
2022-11-29 07:38:29 -0800

    get node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,18 @@
+#include "list.h"
+
+/**
+ * get_nodeint_at_index - returns nth node
+ * @head: head
+ * @index: index
+ * Return: nth node
+ */
+
+listint_t *get_nodeint_at_index(list_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	for (i = 0; i < index && head != NULL; i++)
+		head = head->next;
+
+	return (head);
+}
