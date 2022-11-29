fea5f5caaef9a518423bad1976fcd6ba12b171a5
obinna ekenonu <obinnaekenonu@gmail.com>
2022-11-29 13:21:36 -0800

    free list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,7 +1,7 @@
-#include "list.h"
+#include "lists.h"
 
 /**
- * free_listint - frees a linked list
+ * free_listint - free list
  * @head: head
  * Return: nothing
  */
@@ -11,7 +11,7 @@ void free_listint(listint_t *head)
 	listint_t *temp;
 
 	while ((temp = head) != NULL)
-	{	
+	{
 		head = head->next;
 		free(temp);
 	}
