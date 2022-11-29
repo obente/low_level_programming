cf219b2210d58f91f1a4be67ee9d7f5461d31c8b
obinna ekenonu <obinnaekenonu@gmail.com>
2022-11-29 05:32:50 -0800

    refactored

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,20 +1,20 @@
-
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a list.
- * @h: head of list.
- * Return: numbers of nodes.
+ * print_listint - print element of linked list
+ * @h: list head node
+ * Return: count of nodes
  */
+
 size_t print_listint(const listint_t *h)
 {
-	size_t n_nodes = 0;
+	size_t node_count = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		n_nodes++;
+		node_count++
 	}
-	return (n_nodes);
+	return (node_count);
 }
