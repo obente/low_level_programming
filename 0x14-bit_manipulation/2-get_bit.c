80f69ef6dfb0fd01b7a9b1824ae7b4cea1ff2954
obinna ekenonu <obinnaekenonu@gmail.com>
2022-12-01 14:11:59 -0800

    bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * get_bit - return bit value
+ * @n: n
+ * @index: index
+ * Return: bit value
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int il
+
+		if (n == 0 && index < 64)
+			return (0);
+
+	for (i = 0; i <= 63; n >>= 1, i++)
+	{
+		if (index == 1)
+			return (n & i);
+	}
+	return (-1);
+}
